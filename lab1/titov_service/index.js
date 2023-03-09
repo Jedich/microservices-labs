3e82a3c93738ddb06439788591266eb65519c805
egortitov <egor.titovt@gmail.com>
2023-03-09 21:57:51 +0200

    titov_service

---
+++ lab1/titov_service/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const hostname = "0.0.0.0";
+const port = 3000;
+
+//Create HTTP server and listen on port 3000 for requests
+app.get('/', (req, res) => {
+	res.statusCode = 200;
+	res.send('Hello World!');
+})
+
+app.get('/foo', (req, res) => {
+	res.send('footest');
+})
+
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+app.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
